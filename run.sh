a011438640498f78b18bc035584dd26e0892ad52
Ilya Zolotukhin <zolotuhin_ii@bw-sw.com>
2017-05-24 11:46:28 +0700

    Flag added: "-d"

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --restart=always --privileged -v /var/run/docker.sock:/var/run/docker.sock -p 8888:80 --name haproxy-balancer docker-gen-haproxy-branch
+docker run -d --restart=always --privileged -v /var/run/docker.sock:/var/run/docker.sock -p 8888:80 --name haproxy-balancer docker-gen-haproxy-branch
